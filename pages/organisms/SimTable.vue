50507dd70e12a9c111bb52817d925890b96c1630
Scott Nielson <Scott-Nielson@hotmail.com>
2019-12-27 10:58:37 -0700

    Converted pages/SimTable to typescript.

--- pages/organisms/SimTable.vue
+++ pages/organisms/SimTable.vue
@@ -24,15 +24,15 @@
                         :fontSize="fontSize"
                 >
                     <template slot-scope="scope" slot="actionCommon">
-                        <button size="mini" @click="edit(scope.index, scope.row)">Edit</button>
-                        <button size="mini" @click="del(scope.index, scope.row)">Delete</button>
+                        <button class="btn-sm btn-primary" style="margin-right: 10px" size="mini" @click="edit(scope.index, scope.row)">Edit</button>
+                        <button class="btn-sm btn-primary" size="mini" @click="del(scope.index, scope.row)">Delete</button>
                     </template>
                 </sim-table>
 
 
                 <div class="row">
                     <div class="col-3"></div>
-                    <div class="col-4">
+                    <div class="col-2">
                         <sim-select
                                 v-model="fontSize"
                                 :value="fontSize"
@@ -41,11 +41,17 @@
                                 :items="fontSizes"
                                 :textCentered=true
                                 width="100px"
-                                size="sm"
+                                size="md"
                         ></sim-select>
                     </div>
-                    <div class="col-2" style="margin-top: 16px">
-                        <a class='btn-link' href="https://www.npmjs.com/package/vue-virtual-table">  Documentation  </a>
+                    <div class="col-3" style="margin-top: 20px">
+                        <input type="checkbox"
+                               id="textCentered"
+                               :checked="lineNumShown" @click='changeShowLineNumber' style="padding-left: 10px; cursor: pointer">
+                        <label for="textCentered" style="margin-left: 3px">{{ lineNumLabel }}</label>
+                    </div>
+                    <div class="col-2" style="margin-top: 18px">
+                        <a class='btn-link' style="color: blue" href="https://www.npmjs.com/package/vue-virtual-table">  Documentation  </a>
                     </div>
                 </div>
                 <hr>
@@ -60,6 +66,13 @@
     import SimSelect from '~/components/molecules/SimSelect.vue';
     import { _uuid } from "~/components/organisms/SimTable/utils/index.js";
 
+    export interface dataInterfaceObj {
+        user: string;
+        age: number;
+        city: string;
+        ageDays: number;
+    }
+
     @Component({
         components: {
             SimTable,
@@ -72,6 +85,8 @@
         dataChanged: number = 0
         focused: boolean = false
         newValue: string = ''
+        lineNumShown: boolean = true
+        lineNumLabel: string = 'Hide Line Number Column'
 
         tableConfig: object [] = [
             {prop: "_index", name: "#", width: 80},
@@ -86,16 +101,20 @@
                 prop: "age",
                 name: "Age",
                 numberFilter: true,
+                /// the eClass object will allow you to set differnt colors for age ranges.
                 eClass: {
-                    redTag: '${age}>15 && ${age}<20',
-                    orangeTag: '${age}>19 && ${age}<25',
-                    rustTag: '${age}>24 && ${age}<30',
-                    greenTag: '${age}>29 && ${age}<35',
-                    blueTag: '${age}>34'
+                    redTag: '${age}>15 && ${age}<25',
+                    orangeTag: '${age}>24 && ${age}<35',
+                    rustTag: '${age}>34 && ${age}<50',
+                    greenTag: '${age}>49 && ${age}<66',
+                    blueTag: '${age}>66'
                 }
-
             },
-            {prop: "ageDays", name: "Age in Days", numberFilter: false, sortable: false},
+            {
+                prop: "ageDays",
+                name: "Age in Days",
+                numberFilter: false,
+                sortable: true},
             {
                 prop: "city",
                 name: "City",
@@ -123,17 +142,17 @@
             {prop: "_action", name: "Action", actionName: "actionCommon"},
         ]
 
-        tableData: object [] = [
+        tableData: dataInterfaceObj [] = [
             {user: "a1", age: 20, ageDays: 0, city: "a"},
             {user: "a2", age: 21, ageDays: 0, city: "b"},
             {user: "a3", age: 23, ageDays: 0, city: "a"}
         ]
 
+        /// This is the number of rows that will be randomly generated.
         lineNum: number = 1000
 
-
         tableAttribute: object = {
-            height: 850 as number,
+            height: 650 as number,
             itemHeight: 55 as number,
             minWidth: 1000 as number,
             selectable: true as boolean,
@@ -141,21 +160,28 @@
             bordered: false as boolean,
             hoverHighlight: true as boolean,
             language: "en" as string,
-            sortParam: {col: "age" as string, direction: "desc" as string}
+            sortParam: {col: "user" as string, direction: "asc" as string}
         }
 
         mounted() {
             this.genData()
         }
 
-        updateTableConfig(prop, conf) {
-            const propIndex = this.tableConfig.findIndex(v => v.prop == prop);
-            this.tableConfig.splice(propIndex, 1, conf);
+        changeShowLineNumber() {
+            if (this.lineNumShown) {
+                this.lineNumLabel = 'Show Line Number Column'
+                this.tableConfig.splice(0,1)
+            }
+            else {
+                this.lineNumLabel = 'Hide Line Number Column'
+                this.tableConfig.unshift({prop: "_index", name: "#", width: 80})
+            }
+            this.lineNumShown = !this.lineNumShown
         }
 
         handleSelectionChange(rows) {
             if (rows.length > 0) {
-                !this.isSelected ? alert('The row for user: ' + rows[0].user + ' was selected.') : alert('The row for user: ' + rows[0].user + ' was deselected.');
+                alert('The row for user: ' + rows[0].user + ' was selected.')
             }
         }
 
@@ -168,12 +194,10 @@
         }
 
         del(index, row) {
-            console.log('this.tableData = ', this.tableData)
-            var user = row.user
-            var len = this.tableData.length
-            for (let i = 0; i < len; i++) {
+            const user = row.user
+            for (let i = 0; i < this.tableData.length; i++) {
                 if (user === this.tableData[i].user) {
-                    var temp = this.tableData.splice(i,1);
+                    const temp = this.tableData.splice(i,1);
                     this.dataChanged++
                     break
                 }
@@ -202,24 +226,23 @@
                 "Seattle",
                 "Toronto"
             ];
-            const allData = [];
-            const dataObj = {
-                user: '' as string,
-                age: 0 as number,
-                city: '' as string,
-                ageDays: 0 as number,
-            }
+
+            this.tableData = []
+            let user = '' as string
+            let age = 0 as number
+            let ageDays = 0 as number
+            let city = '' as string
 
             for (let i = 0; i < len; i++) {
-                dataObj.user = "u" + _uuid()
-                dataObj.age = Math.floor((78 - 16) * Math.random()) + 16;
+                user = "u" + _uuid()
+                age = Math.floor((78 - 16) * Math.random()) + 16;
                 const cityIndex = i % 14;
-                dataObj.city = cts[cityIndex];
-                dataObj.ageDays = Math.round(dataObj.age * 365.23 / .12) / 100;
-                console.log('dataObj = ', dataObj)
-               allData.push(dataObj);
+                city = cts[cityIndex];
+                ageDays = Math.round(age * 365.23 / .12) / 100;
+                ageDays = Math.round(age * 365.23 / .12) / 100;
+
+                this.tableData.push({user: user as string, age: age as number, ageDays: ageDays as number, city: city as string})
             }
-            this.tableData = allData
             this.dataChanged++
         }
 
